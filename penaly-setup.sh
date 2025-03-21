fb605e6c9800469957bb468438300b5fc51b1be2
nautique69 <dave@almond.me.uk>
2025-03-21 14:46:10 +0000

    Update penaly-setup.sh

--- penaly-setup.sh
+++ penaly-setup.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
-
-sudo raspi-config nonint do_boot_behaviour B2 # boot to console and log in
-
+sudo raspi-config nonint do_boot_behaviour B2
 sudo apt-get update -qq
-
 sudo apt-get install --no-install-recommends xserver-xorg-video-all xserver-xorg-input-all xserver-xorg-core xinit x11-xserver-utils chromium-browser unclutter
