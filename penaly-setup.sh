2b37113dc563fbb8c3f148ace70c15829606eef9
nautique69 <dave@almond.me.uk>
2025-03-21 14:31:29 +0000

    Update penaly-setup.sh

--- penaly-setup.sh
+++ penaly-setup.sh
@@ -4,13 +4,9 @@ sudo raspi-config nonint do_boot_behaviour B2 # boot to console and log in
 
 sudo apt-get update -qq
 
-sudo apt-get install --no-install-recommends xserver-xorg-video-all \
-  xserver-xorg-input-all xserver-xorg-core xinit x11-xserver-utils \
-  chromium-browser unclutter
+sudo apt-get install --no-install-recommends xserver-xorg-video-all xserver-xorg-input-all xserver-xorg-core xinit x11-xserver-utils chromium-browser unclutter
 
 # update bash profile
-
-
 cat << EOF >> .profile
 if [ -z $DISPLAY ] && [ $(tty) = /dev/tty1 ]
 then
