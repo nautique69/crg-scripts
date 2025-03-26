8f4a0ff3928be3c43f31878a9ca9a650d587e090
Dave Almond <david.almond@rradar.com>
2025-03-26 16:37:06 +0000

    1st try jamstats

---
+++ jamstats-setup.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+sudo raspi-config nonint do_boot_behaviour B4
+sudo raspi-config nonint do_wayland W1
+
+sudo apt-get update
+sudo apt-get install -y libgtk-2-dev python3-pip python3-venv
+
+mkdir ~/jamstats
+cd scoreboard
+pip install jamstats
+cd ..
+
+cat > run-jamstats.sh <<EOF
+#!/bin/bash
+cd ~/jamstats
+python3 -m jamstats --scoreboardserver scoreboard.local:80
+EOF
+chmod +x ./run-jamstats.sh
+
+mkdir -p /home/pi/.config/lxsession/LXDE-pi/
+echo "/home/pi/run-jamstats.sh" >> /home/pi/.config/lxsession/LXDE-pi/autostart
+
+sudo reboot
